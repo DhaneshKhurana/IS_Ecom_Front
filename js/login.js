6064344dae00d3b715412c3bcd45eb953ec069de
Dhanesh Khurana <dhaneshkhurana016@gmail.com>
2023-08-27 21:06:59 +0530

    created login and contact us module

--- js/login.js
+++ js/login.js
@@ -1,14 +1,17 @@
 function validateCredentials() {
-  let id = document.getElementById("email").textContent.toLowerCase();
-  let pwd = document.getElementById("pwd").textContent;
+  let id = document.getElementById("email").value.toLowerCase();
+  let pwd = document.getElementById("pwd").value;
+  console.log("id and password", id, pwd);
 
   if (id === "admin@admin.com" && pwd == 123456) {
     const successToast = document.getElementById("successToast");
+    console.log("sucessToast found", successToast);
     const toast = new bootstrap.Toast(successToast);
     toast.show();
-  }else{
-    const unsucsessToast = document.getElementById("unsucsessToast");
-    const toast = new bootstrap.Toast(unsucsessToast);
+  } else {
+    const unsuccessToast = document.getElementById("unsuccessToast");
+    console.log("unsucessToast found", unsuccessToast);
+    const toast = new bootstrap.Toast(unsuccessToast);
     toast.show();
   }
 }
